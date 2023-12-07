732c9417456654fe185edd412e2ffad6278f5ce7
sury <laeaeaeaen@gmail.com>
2023-12-07 23:37:27 +0530

    fixed OTP

--- app/api/user/new/route.js
+++ app/api/user/new/route.js
@@ -7,6 +7,10 @@ import User from "@models/user"
 export async function POST(request) {
   const newUser = await request.json()
   console.log(newUser);
+  if(newUser.email.split('@')[1] != "lnmiit.ac.in"){
+    console.log("user add failed");
+    return new Response("NON LNMIIT EMAIL");
+  }
   if(!(await User.findOne({email: newUser.email}))){
     await User.create({
         email: newUser.email,
